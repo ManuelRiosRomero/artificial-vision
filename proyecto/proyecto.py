3f3a472272751198617ce0dcd13f56bff42d4107
Manuel Rios <malejandrorios@javeriana.edu.co>
2022-11-21 19:18:37 -0500

    fixed file structure

--- proyecto/proyecto.py
+++ proyecto/proyecto.py
@@ -1,6 +1,6 @@
 import cv2 as cv
 import numpy as np
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import os
 
 def down_scale(src, title):
@@ -49,68 +49,68 @@ def sobel(src):
     return grad
 
 
-imA= cv.imread("img_a.jpeg",0)
-imB= cv.imread("img_b.jpeg",0)
-imC= cv.imread("img_c.jpeg",0)
-imD= cv.imread("img_d.jpeg",0)
-imE= cv.imread("img_e.jpeg",0)
+imA= cv.imread("SourceImgs/img_a.jpeg",0)
+imB= cv.imread("SourceImgs/img_b.jpeg",0)
+imC= cv.imread("SourceImgs/img_c.jpeg",0)
+imD= cv.imread("SourceImgs/img_d.jpeg",0)
+imE= cv.imread("SourceImgs/img_e.jpeg",0)
 
 #GAUSS DOWN
-srcA = down_scale(imA, "GaussA.jpg")
-srcB = down_scale(imB, "GaussB.jpg")
-srcC = down_scale(imC, "GaussC.jpg")
-srcD = down_scale(imD, "GaussD.jpg")
-srcE = down_scale(imE, "GaussE.jpg")
+srcA = down_scale(imA, "Gauss/GaussA.jpg")
+srcB = down_scale(imB, "Gauss/GaussB.jpg")
+srcC = down_scale(imC, "Gauss/GaussC.jpg")
+srcD = down_scale(imD, "Gauss/GaussD.jpg")
+srcE = down_scale(imE, "Gauss/GaussE.jpg")
 
 #SOBEL
 kernel_size = 3
 kernel = np.ones((kernel_size, kernel_size), dtype=np.float64)
 #--------------------------------------#
 soa=sobel(srcA)
-cv.imwrite("bordeA.png",soa)
+cv.imwrite("bordes/bordeA.png",soa)
 di1 = cv.dilate(soa,kernel,iterations = 1)
-cv.imwrite("diA.png",di1)
+cv.imwrite("dilatar1/diA.png",di1)
 sum1=cv.subtract(di1,srcA)
-cv.imwrite("reA.png",sum1)
+cv.imwrite("subtract/reA.png",sum1)
 dil1 = cv.dilate(sum1,kernel,iterations = 1)
-cv.imwrite("dilA.png",dil1)
+cv.imwrite("dilatar2/dilA.png",dil1)
 #------------------------------------#
 sob=sobel(srcB)
-cv.imwrite("bordeB.png",sob)
+cv.imwrite("bordes/bordeB.png",sob)
 di2 = cv.dilate(sob,kernel,iterations = 1)
-cv.imwrite("diB.png",di2)
+cv.imwrite("dilatar1/diB.png",di2)
 sum2=cv.subtract(di2,srcB)
-cv.imwrite("reB.png",sum2)
+cv.imwrite("subtract/reB.png",sum2)
 dil2 = cv.dilate(sum2,kernel,iterations = 1)
-cv.imwrite("dilB.png",dil2)
+cv.imwrite("dilatar2/dilB.png",dil2)
 
 #----------------------------------#
 soc=sobel(srcC)
-cv.imwrite("bordeC.png",soc)
+cv.imwrite("bordes/bordeC.png",soc)
 di3 = cv.dilate(soc,kernel,iterations = 1)
-cv.imwrite("diC.png",di3)
+cv.imwrite("dilatar1/diC.png",di3)
 sum3=cv.subtract(di3,srcC)
-cv.imwrite("reC.png",sum3)
+cv.imwrite("subtract/reC.png",sum3)
 dil3 = cv.dilate(sum3,kernel,iterations = 1)
-cv.imwrite("dilC.png",dil3)
+cv.imwrite("dilatar2/dilC.png",dil3)
 #-----------------------------------#
 sod=sobel(srcD)
-cv.imwrite("bordeD.png",sod)
+cv.imwrite("bordes/bordeD.png",sod)
 di4 = cv.dilate(sod,kernel,iterations = 1)
-cv.imwrite("diD.png",di4)
+cv.imwrite("dilatar1/diD.png",di4)
 sum4=cv.subtract(di4,srcD)
-cv.imwrite("reD.png",sum4)
+cv.imwrite("subtract/reD.png",sum4)
 dil4 = cv.dilate(sum4,kernel,iterations = 1)
-cv.imwrite("dilD.png",dil4)
+cv.imwrite("dilatar2/dilD.png",dil4)
 #-----------------------------------#
 soe=sobel(srcE)
-cv.imwrite("bordeE.png",soe)
+cv.imwrite("bordes/bordeE.png",soe)
 di5 = cv.dilate(soe,kernel,iterations = 1)
-cv.imwrite("diE.png",di5)
+cv.imwrite("dilatar1/diE.png",di5)
 sum5=cv.subtract(di5,srcE)
-cv.imwrite("reE.png",sum5)
+cv.imwrite("subtract/reE.png",sum5)
 dil5 = cv.dilate(sum5,kernel,iterations = 1)
-cv.imwrite("dilE.png",dil5)
+cv.imwrite("dilatar2/dilE.png",dil5)
 #--------------------------------------#
 #no save
 #os.remove('diA.png')
